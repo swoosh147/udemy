a801dd21f6cd83367d833a20167f64ac4beca9ec
Max Savinov <maxisavinov147@gmail.com>
2020-03-13 15:21:03 +0300

    add index.html and script.js on 1-st block

--- step 1/script.js
+++ step 1/script.js
@@ -1,25 +1,15 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+'use strict'
 
-let appData = {
-    budget: money,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    timeData: time,
-    savings: false
-};
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется?", ''),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt("Во сколько обойдется?", '');
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
-
-// Важно! В последних версиях браузеров поведение работы со свойствами объектов поменялось. 
-// Теперь синтаксис через точку может не работать. Поэтому выше я использовал квадратные скобки. 
-// Если вы это читаете - то в скором времени обновится и видео в самом курсе. Спасибо за внимание!
-
-alert(appData.budget / 30);
+var showType = typeof(null);
