3a533eacec61e58b9b4516c1a91c1e90188a965e
Max Savinov <maxisavinov147@gmail.com>
2020-04-19 13:52:00 +0300

    Add basic Object Oriented programming

--- lesson 4/js.js
+++ lesson 4/js.js
@@ -1,26 +1,49 @@
 ' use strict ';
 
-function first() {
-    setTimeout(function() {
-            console.log(1);
-        },
-        500);
+// function first() {
+//     setTimeout(function() {
+//             console.log(1);
+//         },
+//         500);
+// }
+
+// function second() {
+//     console.log(2)
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//     console.log("Я учу " + lang);
+//     callback();
+// }
+
+// function done() {
+//     console.log("Я прошёл 3-й урок");
+// }
+
+// learnJS("JavaScript", done);
+
+let arr = [1, 15, 4, 7, 22]
+
+arr.sort(sortFunc);
+
+console.log(arr);
+
+function sortFunc(a, b) {
+    return b - a;
 }
 
-function second() {
-    console.log(2)
-}
 
-first();
-second();
 
-function learnJS(lang, callback) {
-    console.log("Я учу " + lang);
-    callback();
-}
+let soldier = {
+    health: 400,
+    armor: 100
+};
 
-function done() {
-    console.log("Я прошёл 3-й урок");
-}
+let john = {
+    health: 100
+};
 
-learnJS("JavaScript", done);
+john.__proto__ = soldier;
