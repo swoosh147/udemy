553e052710665e1cc26831c3a0dc8fa801d6b097
Max Savinov <maxisavinov147@gmail.com>
2020-04-19 12:47:23 +0300

    callback function

--- lesson 4/js.js
+++ lesson 4/js.js
@@ -1,9 +1,26 @@
 ' use strict ';
-let testNumber = 20;
 
-function showFitstMessage(text) {
-    alert("Hello!");
-    console.log(testNumber);
+function first() {
+    setTimeout(function() {
+            console.log(1);
+        },
+        500);
 }
 
-showFitstMessage('text');
+function second() {
+    console.log(2)
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log("Я учу " + lang);
+    callback();
+}
+
+function done() {
+    console.log("Я прошёл 3-й урок");
+}
+
+learnJS("JavaScript", done);
