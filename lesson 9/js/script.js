33d3b7809a0acbcf0dfe05251cc5da7f12ef5a73
Max Savinov <maxisavinov147@gmail.com>
2020-04-22 17:56:14 +0300

    Add lesson 10 practice. (made in folder 9)

--- lesson 9/js/script.js
+++ lesson 9/js/script.js
@@ -42,4 +42,62 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Timer
+
+    let deadLine = '2020-04-23';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            hours = Math.floor(t / (1000 * 60 * 60)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
+        }
+
+
+        return {
+            'totol': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return "0" + num;
+                } else return num;
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+
+            if (t.totol < 0) {
+                clearInterval(timeInterval);
+            }
+
+        }
+    }
+
+    setClock('timer', deadLine);
+
+
 });
