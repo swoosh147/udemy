06ba501077c2c48047444f07ccebd6450a871953
Max Savinov <maxisavinov147@gmail.com>
2020-04-27 14:26:07 +0300

    minor fix lesson 11

--- lesson 11/script.js
+++ lesson 11/script.js
@@ -22,23 +22,56 @@
 
 //ES6 format
 
-class User {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        this.human = true;
-    }
-    hello() {
-        console.log(`Hello! ${this.name}`);
-    }
-    exit() {
-        console.log(`User ${this.name} exit`);
-    }
-}
+// class User {
+//     constructor(name, id) {
+//         this.name = name;
+//         this.id = id;
+//         this.human = true;
+//     }
+//     hello() {
+//         console.log(`Hello! ${this.name}`);
+//     }
+//     exit() {
+//         console.log(`User ${this.name} exit`);
+//     }
+// }
 
-let ivan = new User('Ivan', 10);
-let alex = new User('Alex', 25);
-console.log(ivan);
-console.log(alex);
-ivan.hello();
-alex.exit();
+// let ivan = new User('Ivan', 10);
+// let alex = new User('Alex', 25);
+// console.log(ivan);
+// console.log(alex);
+// ivan.hello();
+// alex.exit();
+
+
+// let user = {
+//     name: 'John'
+// };
+
+// function sayName() {
+//     console.log(this);
+//     console.log(this.name);
+// }
+
+// console.log(sayName.call(user));
+// console.log(sayName.apply(user));
+
+
+// function count(num) {
+//     return this * num;
+// }
+
+// let double = count.bind(2);
+
+// console.log(double(3));
+// console.log(double(10));
+
+
+let bytton = document.querySelector('button');
+console.log(bytton);
+
+bytton.addEventListener('click', function() {
+    alert(this);
+    console.log(this);
+    this.style.backgroundColor = 'red';
+});
