c416c3556072b73d10ef1a0b300b6062debad5b7
Max Savinov <maxisavinov147@gmail.com>
2020-04-28 23:36:42 +0300

    Add JSON basic methods

---
+++ lesson 13/script.js
@@ -0,0 +1,12 @@
+let options = {
+    width: 1600,
+    height: 500,
+    background: "red",
+    font: {
+        size: '16px',
+        color: '#fff'
+    }
+
+};
+
+console.log(JSON.parse(JSON.stringify(options)));
