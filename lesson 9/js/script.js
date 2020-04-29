3f5127818a360e92cce524516b1612669be606e7
Max Savinov <maxisavinov147@gmail.com>
2020-04-30 00:17:39 +0300

    Sending script implementation form data

--- lesson 9/js/script.js
+++ lesson 9/js/script.js
@@ -141,5 +141,53 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // The project moved to the mump, a copy in this file
+
+    // form
+    let message = {
+        loading: 'Загрузка...',
+        sucsess: 'Спасибо, скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(true);
+        form.appendChild(statusMessage);
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); Сделали для php файла, нижк для JSON формата
+        request.setRequestHeader('Content-type', 'application/json', 'charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.sucsess;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i <= input.length; i++) {
+            input[i].value = '';
+        }
+    });
 
 });
