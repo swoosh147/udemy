ca7b74e588588d94236f28a64396ee71cd317d17
Max Savinov <maxisavinov147@gmail.com>
2020-05-06 23:46:36 +0300

    add try-catch-finally lesson

--- lesson 17/script.js
+++ lesson 17/script.js
@@ -16,3 +16,28 @@ let serializedPerson = JSON.stringify(person);
 localStorage.setItem("Alex", serializedPerson);
 
 console.log(JSON.parse(localStorage.getItem('Alex')));
+
+let json = '{"id":2}';
+
+try {
+    // console.log('Normal');
+    // console.log(a);
+    // console.log('continue');
+    let user = JSON.parse(json);
+    console.log(user);
+
+    if (!user.name) {
+        throw new Error("В этих данных нет имени");
+    }
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+    console.log('error');
+
+    console.log(`Мы получили ошибку: ${error.name}`);
+} finally {
+    console.log('Я выполнюсь всегда');
+}
+
+console.log('continue again');
