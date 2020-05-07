ec24a2cf7319213fbd45721bcbba60b05b7d5bfa
Max Savinov <maxisavinov147@gmail.com>
2020-05-07 12:55:44 +0300

    add jQery lesson

---
+++ lesson 17/Homework/script.js
@@ -0,0 +1,15 @@
+console.log("Hi");
+
+$(document).ready(function() {
+
+    $('.main_btna, .main_btn, a[href="#sheldure"]').click(function() {
+        $('.overlay').fadeIn(1000);
+        $('.modal').slideDown(1000);
+    });
+
+    $('.close').click(function() {
+        $('.modal').slideUp(1000);
+        $('.overlay').fadeOut(1000);
+    });
+
+});
