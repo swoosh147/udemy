d621ed299cd96bad5ca1a0ec8c620181ed09f52b
Max Savinov <maxisavinov147@gmail.com>
2020-05-23 01:32:20 +0300

    added ie-10 support

--- react_lesson 1/src/index.js
+++ react_lesson 1/src/index.js
@@ -1,7 +1,77 @@
-import Slider, { button } from './script'
+"use strict";
 
-const slider = new Slider(400, 200, 5);
+require("core-js/modules/es7.symbol.async-iterator");
 
+require("core-js/modules/es6.symbol");
+
+require("core-js/modules/web.dom.iterable");
+
+require("core-js/modules/es6.array.iterator");
+
+require("core-js/modules/es6.object.to-string");
+
+require("core-js/modules/es6.string.iterator");
+
+require("core-js/modules/es6.weak-map");
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+require("core-js/modules/web.dom.iterable");
+
+var _script = _interopRequireWildcard(require("./script"));
+
+function _getRequireWildcardCache() {
+  if (typeof WeakMap !== "function") return null;
+  var cache = new WeakMap();
+
+  _getRequireWildcardCache = function _getRequireWildcardCache() {
+    return cache;
+  };
+
+  return cache;
+}
+
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  }
+
+  if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") {
+    return {
+      default: obj
+    };
+  }
+
+  var cache = _getRequireWildcardCache();
+
+  if (cache && cache.has(obj)) {
+    return cache.get(obj);
+  }
+
+  var newObj = {};
+  var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+
+      if (desc && (desc.get || desc.set)) {
+        Object.defineProperty(newObj, key, desc);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+  }
+
+  newObj.default = obj;
+
+  if (cache) {
+    cache.set(obj, newObj);
+  }
+
+  return newObj;
+}
+
+var slider = new _script.default(400, 200, 5);
 slider.whoAmI();
-
-console.log(button());
+console.log((0, _script.button)());
--- react_lesson 1/src/script.js
+++ react_lesson 1/src/script.js
@@ -1,46 +1,109 @@
+"use strict";
+
+require("core-js/modules/es7.symbol.async-iterator");
+
+require("core-js/modules/es6.symbol");
+
+require("core-js/modules/es6.object.set-prototype-of");
+
+require("core-js/modules/es6.regexp.to-string");
+
+require("core-js/modules/es6.object.to-string");
+
+require("core-js/modules/es6.reflect.construct");
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function () { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.button = button;
+exports.default = void 0;
+
 function button() {
-    return 'button';
+  return 'button';
 }
 
-class Slider {
-    constructor(width, height, count) {
-        this.width = width;
-        this.height = height;
-        this.count = count;
-    }
-    nextSlide() {
-        console.log('moving forvard')
-    }
-    prevSlide() {
-        console.log('back forvard')
-    }
-    whoAmI() {
-        console.log(`width: ${this.width}, height: ${this.height}, count: ${this.count} `);
-    }
+var Slider = /*#__PURE__*/function () {
+  function Slider(width, height, count) {
+    _classCallCheck(this, Slider);
 
-}
+    this.width = width;
+    this.height = height;
+    this.count = count;
+  }
 
-// const slider = new Slider(600, 400, 5),
+  _createClass(Slider, [{
+    key: "nextSlide",
+    value: function nextSlide() {
+      console.log('moving forvard');
+    }
+  }, {
+    key: "prevSlide",
+    value: function prevSlide() {
+      console.log('back forvard');
+    }
+  }, {
+    key: "whoAmI",
+    value: function whoAmI() {
+      console.log("width: ".concat(this.width, ", height: ").concat(this.height, ", count: ").concat(this.count, " "));
+    }
+  }]);
+
+  return Slider;
+}(); // const slider = new Slider(600, 400, 5),
 //     someSlider = new Slider(400, 200, 10);
-
 // slider.whoAmI();
 // someSlider.whoAmI();
 
-class AutoSlider extends Slider {
-    constructor(width, height, count, auto) {
-        super(width, height, count);
-        this.auto = auto;
-    }
-    play() {
-        console.log(`Autoplay: ${this.auto}`);
-    }
-}
 
-// const slider = new AutoSlider(500, 500, 4, true);
+var AutoSlider = /*#__PURE__*/function (_Slider) {
+  _inherits(AutoSlider, _Slider);
 
+  var _super = _createSuper(AutoSlider);
+
+  function AutoSlider(width, height, count, auto) {
+    var _this;
+
+    _classCallCheck(this, AutoSlider);
+
+    _this = _super.call(this, width, height, count);
+    _this.auto = auto;
+    return _this;
+  }
+
+  _createClass(AutoSlider, [{
+    key: "play",
+    value: function play() {
+      console.log("Autoplay: ".concat(this.auto));
+    }
+  }]);
+
+  return AutoSlider;
+}(Slider); // const slider = new AutoSlider(500, 500, 4, true);
 // slider.whoAmI();
 // slider.play();
 
-export { button };
 
-export default Slider;
+var _default = Slider;
+exports.default = _default;
