29aa12f7b88ab124ecb18bd9bbf4b614db5af5a5
Max Savinov <maxisavinov147@gmail.com>
2020-05-23 16:19:37 +0300

    add AppHeader, SearchPanel

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
 
 const App = () => {
-    return (<h1>Hello, world</h1>)
+    return (
+        <div className="app">
+            <AppHeader/>
+        <div className="search-panel d-flex">
+            <SearchPanel/>
+        </div>
+        </div>
+    )
 }
 
 export default App;
