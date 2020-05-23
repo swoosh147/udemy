3475fda49cb776f30799f0302b1b6c415bf4cf4f
Max Savinov <maxisavinov147@gmail.com>
2020-05-23 15:53:50 +0300

    create basic react app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/app/';
 
-ReactDOM.render( <React.StrictMode >
-    <App / >
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+
+// const Header = () => {
+//     // const scr = "<script>alert('Error!')</script>"
+//     return <h2 > Hello, world!</h2>
+// }
+// const Field = () => {
+//     const holder = 'Enter here'
+//     const styleField ={
+//         width: '300px'
+// }
+//     return <input type = 'Text'
+//                  placeholder = {holder}
+//                  autoComplite = ""
+//                  style = {styleField}/>
+// }
+// const Button = () => {
+//     const text = 'Log in'; 
+//     const logged = true;
+//     // const res = () =>{
+//     //     return 'Log in, please'
+//     // }
+//  return <button>{logged ? 'Enter' : text}</button>
+// }
+
+// const App = () => {
+//     return ( <div>
+//         <Header />
+//         <Field />
+//         <Button />
+//         </div>
+//     )
+// }
+
+ReactDOM.render( <App /> , document.getElementById('root'));
