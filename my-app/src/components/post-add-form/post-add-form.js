3c6644de34d484c68eb626e06e27679e39b70045
Max Savinov <maxisavinov147@gmail.com>
2020-05-26 14:52:52 +0300

    add post add event

--- my-app/src/components/post-add-form/post-add-form.js
+++ my-app/src/components/post-add-form/post-add-form.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './post-add-form.css';
 
-const PostAddForm = () => {
+const PostAddForm = ({onAdd}) => {
     return (
-        <form className="bottom-panel d-flex">
+        <div className="bottom-panel d-flex">
             <input
                 type="text"
                 placeholder="О чём вы думаете сейчас?"
                 className="form-control new-post-label"
             />
-            <button type='submit' className='btn btn-outline-secondary'>
+            <button 
+                type='submit' 
+                className='btn btn-outline-secondary'
+                onClick={() => onAdd('Hello')}>
                 Добавить
             </button>
-        </form>
+        </div>
     )
 }
 
