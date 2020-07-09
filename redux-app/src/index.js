b3fd9562ffaf919facbd236ffc80a9bd3293b752
Max Savinov <maxisavinov147@gmail.com>
2020-07-09 16:19:26 +0300

    add first redux lesson, crete store and use disratch

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+console.log('Hello');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const initialState = 0;
+
+const reduser = (state = 0, action) => {
+//   if(action.type === 'INC'){
+//     return state + 1;
+//   }
+//   return 0;
+// }
+    switch(action.type){
+      case 'INC':
+        return state + 1;
+      case 'DEC':
+        return state - 1;
+      case 'RND':
+        return state + action.value;
+      default:
+        return state;
+    }
+}
+
+// const inc = () =>{
+//   return {
+//     type: 'INC'
+//   }
+// }
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', value});
+
+const store = createStore(reduser);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// console.log(store.getState());
+// store.dispatch({type: 'INC'});
+// console.log(store.getState());
+// store.dispatch({type: 'INC'});
+// console.log(store.getState());
+// store.dispatch({type: 'INC'});
+// console.log(store.getState());
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+})
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+})
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(value));
+})
+
+const update = () =>{
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
+
+// let state = reduser(undefined, {});
+
+// state = reduser(state, {type: 'INC'});
+// console.log(state);
+
+// state = reduser(state, {type: 'INC'});
+// console.log(state);
