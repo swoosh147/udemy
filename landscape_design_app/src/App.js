e9a3171a23dfd0ebe551b5f1c2b83e1f9d5c263c
Max Savinov <maxisavinov147@gmail.com>
2020-07-15 15:06:43 +0300

    /

--- landscape_design_app/src/App.js
+++ landscape_design_app/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="root" className="jumbotron"></div>
+    <h1 id='counter'>База данных распений и тестирование API</h1>
+    <button id='dec' className='btn btn-primary'>prev</button>
+    <button id='inc' className="btn btn-primary">next</button>
+    <button id='rnd' className="btn btn-primary">random</button>
       </header>
     </div>
   );
