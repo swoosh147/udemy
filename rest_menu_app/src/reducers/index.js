d89e8e26fd6da9b36f17896e1f61658b73e02d01
Max Savinov <maxisavinov147@gmail.com>
2020-07-31 18:18:04 +0300

    add application template. add the main project structure, add the architecture to the application, completed the homework to create the initial routing

---
+++ rest_menu_app/src/reducers/index.js
@@ -0,0 +1,17 @@
+
+const initialState = {
+    menu: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'MENU_LOADED':
+            return {
+                menu: action.payload
+            };
+        default: 
+            return state;
+    }
+}
+
+export default reducer;
